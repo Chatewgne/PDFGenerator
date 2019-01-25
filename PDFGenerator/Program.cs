31aaa69f34351f56bb1d5fe752ef6c5a97078e02
Chatewgne <chatewgne@gmail.com>
2019-01-25 13:09:14 +0100

    removed some comments and wrote in README

--- PDFGenerator/Program.cs
+++ PDFGenerator/Program.cs
@@ -3,16 +3,9 @@ using iText.Layout;
 using iText.Layout.Element;
 using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using iText.Kernel.Font;
 using iText.IO.Font.Constants;
 using iText.Layout.Properties;
-using iText.Layout.Borders;
-using iText.Kernel.Colors;
-using System.Threading;
 using System.Diagnostics;
 
 namespace PDFGenerator
@@ -27,9 +20,9 @@ namespace PDFGenerator
             string input = Console.ReadLine();
             string[] lines = File.ReadAllLines(input);
             //retrieve input file name
-            string file_name = input.Substring(input.LastIndexOf("/")).Split('.')[0];
+            string file_name = input.Substring(input.LastIndexOf("\\")).Split('.')[0];
             //create output pdf file in the same folder with same name
-            string output = input.Substring(0, input.LastIndexOf("/")+1) + file_name +".pdf";
+            string output = input.Substring(0, input.LastIndexOf("\\")+1) + file_name +".pdf";
             PdfWriter writer = new PdfWriter(new FileStream(output, FileMode.Create));
             //create objects to write in the pdf file
             PdfDocument pdf = new PdfDocument(writer);
@@ -49,10 +42,12 @@ namespace PDFGenerator
                     {
                         firstCommand = false;
                         par.Add(text);
-                    } //TODO no space after a command - text on one line - fill comes after indent - /eplxain / not \ explain that my fill and unfill command make new paragraphs /that it doesn't handle "no space" scenario
+                    }
                     switch (line)
                     { //apply changes to paragraph or text according to the command
                         case ".large":
+                            document.Add(par);
+                            par = new Paragraph();
                             text = new Text("");
                             text.SetFontSize(30.0f);
                             break;
@@ -74,7 +69,7 @@ namespace PDFGenerator
                             par = new Paragraph();
                             par.SetTextAlignment(TextAlignment.LEFT);
                             break;
-                        case ".italics":
+                        case ".italic":
                             text = new Text("");
                             text.SetItalic();
                             break;
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
-# PDFGenerato
+# PDFGenerator
+
+## Installation
+
+### [Download zip from release](https://github.com/Chatewgne/PDFGenerator/releases)
+
+### Clone from github
+```
+git clone https://github.com/Chatewgne/PDFGenerator
+```
+
+## How to use 
+
+### Running the program
+You will need an input file in the form of a .txt file containing commands and text lines. You can use the "myfile.txt" file available in the project.
+
+Open the solution (PDFGenerator.sln) in Visual Studio and run the application.
+In the console that pops up, enter the path to your input file i.e. : C:\Users\Alex\Documents\myfile.txt
+The program will generate a pdf file with the same name (i.e. myfile.pf) in the same folder.
+
+### Input file formatting 
+The supported commands are the following : 
+.large
+.normal and .regular (identical effects)
+.paragraph
+.fill 
+.nofill
+.bold
+.italic
+.indent X -> replace X by 0 for default indentation, a positive integer offsets to the right **from the default indentation** while a negative integer offsets to the left **from the default indentation**.   
+
+The .indent command must come as the **last command** if several commands in a row are written.
+
+Make sure there are no spaces after a command or it will not be recognized. 
+Make sure there are no linebreaks in a text entry or some parts might be missing in the output pdf.
+
+## Possible improvements
+I couldn't find a quick easy way to add pagination. 
+For the indent command I chose to use the absolute position rather than using the relative position, although it's not exactly what was expected it was easier to implement. 
+I didn't refactor my code, but it is quite short and I think it is clear enough as it is.
